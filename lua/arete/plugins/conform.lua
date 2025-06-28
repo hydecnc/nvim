522c89e51c5e98ba0dd63366e7711b4593318be4
Hyde Yoo <hydeyoo0716@gmail.com>
2025-06-28 15:46:10 +0900

    fix: enable format on save for c/cpp files

--- lua/arete/plugins/conform.lua
+++ lua/arete/plugins/conform.lua
@@ -17,9 +17,8 @@ return {
       notify_on_error = false,
       format_on_save = function(bufnr)
         -- Disable "format_on_save lsp_fallback" for languages that don't
-        -- have a well standardized coding style. You can add additional
-        -- languages here or re-enable it for the disabled ones.
-        local disable_filetypes = { c = true, cpp = true }
+        -- have a well standardized coding style.
+        local disable_filetypes = {}
         if disable_filetypes[vim.bo[bufnr].filetype] then
           return nil
         else
