38fca3dd84e81ea8e69a7827fb1ca5bc8b00343e
Hyde Yoo <hydeyoo0716@gmail.com>
2025-07-02 10:50:14 +0900

    chore: change mini.surround keymaps

--- lua/arete/plugins/mini.lua
+++ lua/arete/plugins/mini.lua
@@ -7,7 +7,17 @@ return {
     },
     config = function()
       require('mini.ai').setup { n_lines = 500 }
-      require('mini.surround').setup()
+      require('mini.surround').setup {
+        mappings = {
+          add = 'gsa', -- Add surrounding in Normal and Visual modes
+          delete = 'gsd', -- Delete surrounding
+          find = 'gsf', -- Find surrounding (to the right)
+          find_left = 'gsF', -- Find surrounding (to the left)
+          highlight = 'gsh', -- Highlight surrounding
+          replace = 'gsr', -- Replace surrounding
+          update_n_lines = 'gsn', -- Update `n_lines`
+        },
+      }
       require('mini.pairs').setup()
       require('mini.diff').setup()
 
