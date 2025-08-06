6ae12513541846cae0ff2a5031be22b0697bbd1a
Hyde Yoo <hydeyoo0716@gmail.com>
2025-08-06 13:13:20 -0400

    feat: add mini.files toggle

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -16,17 +16,19 @@ return {
         },
       }
       require('mini.pairs').setup()
-      require('mini.diff').setup()
-      require('mini.files').setup()
       require('mini.icons').setup()
+      require('mini.diff').setup()
+
+      require('mini.files').setup()
+      MiniFilesToggle = function(...)
+        if not MiniFiles.close() then
+          MiniFiles.open(...)
+        end
+      end
 
       local statusline = require 'mini.statusline'
-      -- set use_icons to true if you have a Nerd Font
       statusline.setup { use_icons = vim.g.have_nerd_font }
 
-      -- You can configure sections in the statusline by overriding their
-      -- default behavior. For example, here we set the section for
-      -- cursor location to LINE:COLUMN
       ---@diagnostic disable-next-line: duplicate-set-field
       statusline.section_location = function()
         return '%2l:%-2v'
@@ -36,7 +38,7 @@ return {
       {
         '<leader>fe',
         function()
-          MiniFiles.open()
+          MiniFilesToggle()
         end,
         desc = 'Mini Files (cwd)',
       },
