c57f28a8776d39881cd9bf87f6152980ef393170
Hyde Yoo <hydeyoo0716@gmail.com>
2025-10-14 22:22:41 -0400

    feat: add support for c/cpp debugging

--- lua/plugins/nvim-dap.lua
+++ lua/plugins/nvim-dap.lua
@@ -24,7 +24,26 @@ return {
     opts = function()
       -- Simple configuration to attach to remote java debug process
       -- Taken directly from https://github.com/mfussenegger/nvim-dap/wiki/Java
-      -- local dap = require 'dap'
+      local dap = require 'dap'
+      dap.adapters.gdb = {
+        type = 'executable',
+        command = 'gdb',
+        args = { '--interpreter=dap', '--eval-command', 'set print pretty on' },
+      }
+      dap.configurations.c = {
+        {
+          name = 'Launch',
+          type = 'gdb',
+          request = 'launch',
+          program = function()
+            return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
+          end,
+          args = {}, -- provide arguments if needed
+          cwd = '${workspaceFolder}',
+          stopAtBeginningOfMainSubprogram = false,
+        },
+      }
+      dap.configurations.cpp = dap.configurations.c
       -- dap.configurations.java = {
       --   {
       --     type = 'java',
